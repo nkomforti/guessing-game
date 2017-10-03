3faf11dd40e68d069deb54e230a57c8e86a8ca15
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-03 13:05:08 -0700

    Generated random number, itialized guess, started loop

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 """A number-guessing game."""
+import random
 
 # Put your code here
 print "Hello, welcome to the guessing game."
 player_name = raw_input("What is your name? ")
 print "Welcome, %s" % (player_name)
+
+random_num = random.randint(1, 100)

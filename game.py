d8cec4dae2b9147231f41ce104149e02881730c2
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-03 14:07:46 -0700

    Got guess, wrote conditional statements to return outcome

--- game.py
+++ game.py
@@ -7,3 +7,18 @@ player_name = raw_input("What is your name? ")
 print "Welcome, %s" % (player_name)
 
 random_num = random.randint(1, 100)
+# why is guess initially set to "None" and not 0 or something else?
+guess = None
+
+while guess != random_num:
+    guess = int(raw_input("Your guess? "))
+
+    if guess != random_num:
+        if guess > random_num:
+            print "Too high"
+        else:
+            print "Too low"
+    else:
+        print "Congratulations, that is correct!"
+
+        
